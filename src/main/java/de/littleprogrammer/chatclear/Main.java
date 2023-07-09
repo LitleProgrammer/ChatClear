8aea451893b187794e8f7262350abaa9156cbd97
linusglimm <linus.glimm06@gmail.com>
2023-07-09 12:47:13 +0200

    Initial commit

---
+++ src/main/java/de/littleprogrammer/chatclear/Main.java
@@ -0,0 +1,17 @@
+package de.littleprogrammer.chatclear;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ChatClear extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
